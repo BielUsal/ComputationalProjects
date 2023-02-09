e15a2f527d10929730f1ee5b526bf5e89ff16df9
Biel <gabdandrade@usal.es>
2023-02-09 22:08:32 +0100

    09/02

--- Julia/PhaseAttempt/phase.jl
+++
@@ -1,9 +0,0 @@
-using Makie 
-epsilon=1
-E=1
-omega=1
-odeSol(x,y) = Point(omega y, epsilon*E*cos(x)) # x'(t) = -x, y'(t) = 2y
-scene = Scene(resolution =(400,400))
-streamplot!(scene, odeSol, -2..2, -2..2, colormap = :plasma, 
-    gridsize= (32,32), arrow_size = 0.07)
-save("odeField.png", scene)
