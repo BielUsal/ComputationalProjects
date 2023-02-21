33371a743efd0910bf50a1366cfa3882932576b0
Biel <gabdandrade@usal.es>
2023-02-21 21:19:45 +0100

    Single-galaxy evolution

--- Julia/GalaxyCollision/GalaxyCollisionFunctions.jl
+++ Julia/GalaxyCollision/GalaxyCollisionFunctions.jl
@@ -1,14 +1,10 @@
-using LinearAlgebra
-using Unitful
-using UnitfulAstro
-
-G = 4.3009E-3 *1u"pc *(km/s)^2 /Msun"
+const G = 4.3009E-3 *1u"pc *(km/s)^2 /Msun"
 function format_parameters(galaxy_args)
     #I didn't have this at first, but this makes all galaxies have an uniform format. Not too crazy
     return Dict(
         "mass" => galaxy_args[1]*1u"Msun",
         "radius"     => galaxy_args[2]*1u"pc",
-        "center_pos" => galaxy_args[3].*1u"pc",
+        "center_pos" => galaxy_args[3].*1u"kpc",
         "center_vel" => galaxy_args[4].*1u"km/s",
         "normal"     => galaxy_args[5],
         "Nᵣ"    => galaxy_args[6],
@@ -31,15 +27,19 @@ function init_disk!(galaxy,dT=1E-4u"yr")
         cosθ = normalize(galaxy["normal"])[3]
         sinθ = √(1-cosθ^2)
         u = [0,0,1] × normalize(galaxy["normal"])
-        u = normalize(u)
+        if norm(u) == 0 
+            Rotation = I
+        else 
+         u = normalize(u)
 
-        Rotation = [
-        u[1]*u[1]*(1-cosθ)+cosθ u[1]*u[2]*(1-cosθ)-u[2]*sinθ u[1]*u[3]*(1-cosθ)+u[1]*sinθ;
+        @show Rotation = [
+            u[1]*u[1]*(1-cosθ)+cosθ u[1]*u[2]*(1-cosθ)-u[2]*sinθ u[1]*u[3]*(1-cosθ)+u[1]*sinθ;
 
-        u[2]*u[1]*(1-cosθ)+u[3]*sinθ u[2]*u[2]*(1-cosθ)+cosθ u[2]*u[3]*(1-cosθ)-u[1]*sinθ;
+            u[2]*u[1]*(1-cosθ)+u[3]*sinθ u[2]*u[2]*(1-cosθ)+cosθ u[2]*u[3]*(1-cosθ)-u[1]*sinθ;
 
-        u[3]*u[1]*(1-cosθ)+u[2]*sinθ u[3]*u[1]*(1-cosθ)+u[1]*sinθ u[3]*u[3]*(1-cosθ)+cosθ
-        ]
+            u[3]*u[1]*(1-cosθ)+u[2]*sinθ u[3]*u[1]*(1-cosθ)+u[1]*sinθ u[3]*u[3]*(1-cosθ)+cosθ
+            ]
+        end
     end
 #-----------------------------------------------------------------------------------------------------------------------------------
     galaxy["star_pos"] = []
@@ -53,9 +53,9 @@ function init_disk!(galaxy,dT=1E-4u"yr")
 
         # Positions 
         vecᵣ = (Rotation * ([rₛ.*cos.(ϕₛ),rₛ.*sin.(ϕₛ),zeros(StarsPerRing)])).*1u"pc"
-        x = galaxy["center_pos"][1].+vecᵣ[1]
-        y = galaxy["center_pos"][2].+vecᵣ[2]
-        z = galaxy["center_pos"][3].+vecᵣ[3]
+        x = ustrip.(u"m",galaxy["center_pos"][1].+vecᵣ[1])#here, we strip our units into our 'integrating' unitlessness because units don't really play nice in arrays of arrays
+        y = ustrip.(u"m",galaxy["center_pos"][2].+vecᵣ[2])
+        z = ustrip.(u"m",galaxy["center_pos"][3].+vecᵣ[3])
 
         # Velocities
         Tₛ = 2π * uconvert.(u"s",sqrt.((rₛ*1u"pc").^3/(G *Introoder["mass"])))
@@ -63,12 +63,81 @@ function init_disk!(galaxy,dT=1E-4u"yr")
         Δϕ = 2π *uconvert(u"s",dT)./Tₛ 
 
         vecᵥ = (Rotation* [(rₛ/(uconvert(u"s",dT)*1u"1/s")).*(cos.(ϕₛ)-cos.(ϕₛ-Δϕ)),rₛ/(uconvert(u"s",dT)*1u"1/s").*(sin.(ϕₛ)-sin.(ϕₛ-Δϕ)),zeros(StarsPerRing)])*1u"pc/s"
-        v₁ = uconvert.(u"km/s",galaxy["center_vel"][1].+vecᵥ[1])
-        v₂ = uconvert.(u"km/s",galaxy["center_vel"][2].+vecᵥ[2])
-        v₃ = uconvert.(u"km/s",galaxy["center_vel"][3].+vecᵥ[3])
-        push!(galaxy["star_pos"],[x,y,z])
-        push!(galaxy["star_vel"],[v₁,v₂,v₃])
+        v₁ = ustrip.(u"m/s",galaxy["center_vel"][1].+vecᵥ[1])
+        v₂ = ustrip.(u"m/s",galaxy["center_vel"][2].+vecᵥ[2])
+        v₃ = ustrip.(u"m/s",galaxy["center_vel"][3].+vecᵥ[3])
+        for j ∈ 1:StarsPerRing
+            push!(galaxy["star_pos"],[x[j],y[j],z[j]])
+            push!(galaxy["star_vel"],[v₁[j],v₂[j],v₃[j]])
+        end
         R += dr
     end 
     galaxy["vel_scale"]=uconvert(u"km/s",√(G*galaxy["mass"]/(0.5*R)))
+end
+
+
+function evolve_disk(galaxy,dT=1e-4u"yr",N_steps=100000,frames=500)
+    divs = trunc(Int64,N_steps/frames)
+    #Integration stuff
+    dT=ustrip(u"s",dT)
+    rₘ=ustrip(u"m",galaxy["softening"]*galaxy["radius"])
+    Nₛ=galaxy["Nₛ"]
+    
+    #Galaxy stuff
+    M = ustrip(u"Msun",galaxy["mass"])
+    R₀ = ustrip.(u"m",galaxy["center_pos"])
+    V₀ = ustrip.(u"m/s",galaxy["center_vel"])
+
+    #star stuff
+    rₛ=galaxy["star_pos"]
+    vₛ=galaxy["star_vel"]
+
+    function Gravity(dq,q,p,t) 
+        r = q[1:3]
+        R = q[4:6]
+        ddr = ustrip(u"m^3/s^2",G*M*1u"Msun")*(R-r)/max(norm(R-r),rₘ)^3
+        ddR = [0,0,0]
+        append!(ddr,ddR)
+        return ddr
+    end
+    Trolleg=[]
+    Integrator=[]
+    for i ∈ 1:Nₛ
+        append!(vₛ[i],V₀)
+        append!(rₛ[i],R₀)
+    end
+    for i ∈ 1:Nₛ
+        push!(Trolleg,SecondOrderODEProblem(Gravity,vₛ[i],rₛ[i],(0,dT*N_steps)))
+        push!(Integrator,init(Trolleg[i],dt=dT,McAte2()))
+    end 
+    snapshot = zeros(frames,Nₛ+1,3)
+    time = zeros(frames)
+        for i ∈ 1:frames
+            for j ∈ 1:Nₛ
+                snapshot[i,j,:] =Integrator[j].u[7:9]*3.240779289444365e-20
+            end
+            snapshot[i,Nₛ+1,:] = Integrator[1].u[10:12]*3.240779289444365e-20
+            time[i]=Integrator[1].t
+
+            for j ∈ 1:divs
+                step!.(Integrator)
+            end
+        end
+    return snapshot,time 
+end
+
+function gif_galaxy(data,time,xlimit=[0,0],ylimit=[0,0],zlimit=[0,0])
+    if ((norm(xlimit) == 0.0) || (norm(ylimit) == 0.0) || (norm(zlimit) == 0.0))
+        xlimit = [minimum(data[:,:,1]),maximum(data[:,:,1])]
+        ylimit = [minimum(data[:,:,2]),maximum(data[:,:,2])]
+        zlimit = [minimum(data[:,:,3]),maximum(data[:,:,3])]
+    end
+    @gif for i ∈ 1:length(time)
+        scatter3d(xlim=xlimit,ylim=ylimit,zlim=zlimit)
+        for j ∈ 1:Introoder["Nₛ"]
+            scatter3d!(data[i:i,j,1],data[i:i,j,2],data[i:i,j,3], color=:red,legends=false,markersize=0.5)
+           # i > trail ? plot3d!(Rotated[j][1,1:i],Rotated[j][2,1:i],Rotated[j][3,1:i]) : nothing
+        end
+        scatter3d!(data[i:i,Introoder["Nₛ"]+1,1],data[i:i,Introoder["Nₛ"]+1,2],data[i:i,Introoder["Nₛ"]+1,3], color=:black,legends=false)
+    end 
 end
