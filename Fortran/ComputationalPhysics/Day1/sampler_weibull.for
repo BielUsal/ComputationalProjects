4d3186bef350d6a388f8b277bd1f18b7ee2cfee6
BielUsal <gabdandrade@usal.es>
2023-09-25 18:13:05 +0200

    25/09

--- Fortran/ComputationalPhysics/Day1/sampler_weibull.for
+++ Fortran/ComputationalPhysics/Day1/sampler_weibull.for
@@ -1,53 +1,53 @@
-      !-----------------------------------------------------------------
+      
+            !-----------------------------------------------------------------
       !Typing and initializations
       Program random 
       Implicit none
       integer N, i, nbin !nbin is the number of bins
       Parameter (N=1E5)
       Parameter (nbin=100)
-      real*8 vector_gen (N),x(N),lambda,P(nbin),alpha,a,b!,vector_read (N),a
-      integer Iterations(N)
+      real*8 vector_gen (N),x(N),lambda,P(nbin)
+      real*8 a,b!,vector_read (N),a
       call srand(1333)
       !-----------------------------------------------------------------
       !Open up the files
-      open(15, file="serieweibull.txt")
+      open(15, file="serielambda1.txt")
       !open(16,file="serie.txt") !read vector example
-      open(17,file="histogramaweibull.txt")
+      open(17,file="histogramalamb1.txt")
       !-----------------------------------------------------------------
+      !This is where we do the actual sampling
       lambda = 1.0d0
       alpha = 1.5d0
       do i =1, N
-      	Iterations(i) = i
-      	vector_gen (i) = rand(0)
-      	x(i) = ((-dlog(vector_gen(i)))**(1/alpha))/lambda
-      	write(15,*) Iterations(i), vector_gen(i), x(i)
-      end do
-      a = 0.00d0
-      b = maxval(x)
-      do i =1,nbin
-      	P(i) = 0
+        vector_gen (i) = rand(0)!This samples from a ~U(0,1)
+        x(i) = ((-dlog(vector_gen(i)))**(1/alpha))/lambda!This transforms it into an ~Weibull(lambda,alpha)
+        write(15,*) i, vector_gen(i), x(i) !This writes it to a file
       end do
       close(15)
-      call Histogram(x,P,0.d0,5.d0)
-      do i = 1,nbin
-        write(17,*) a+dble(i)*(b-a)/dble(nbin), P(i)/N
+      !-----------------------------------------------------------------
+      !Now we do some histogramming. We have made a small little subroutine to do the dirty work so that we can easily use it later
+      a = 0.0d0
+      b = maxval(x) !Min and max value of our representation
+      do i =1,nbin
+        P(i) = 0
       end do
-
-      !do i =1, N
-      !	read(16,*) a, vector_read
-      !end do
-      !close(16)
-      !write(*,*) vector_gen(15),vector_read(15)
+      call Histogram(x,P,a,b)
+      close(17)
       end
 
       subroutine Histogram(vector,bin,a,b)
       integer i,j, N, nbin
       Parameter (N=1E5)
       Parameter (nbin=100)
-      real*8 vector(N),bin(nbin),a,b,h !V is the vector we want to histogram, bin represents the number in each bin
-      h = (b-a)/DBLE(nbin)
+      real*8 vector(N),bin(nbin),a,b,h,x !'vector' is the vector we want to histogram, bin represents the number in each bin
+      h = (b-a)/DBLE(nbin)!This uses a,b and nbin to calculate the length of each bin
       do i =1,N
-      	j = int((vector(i)-a)/h)
-      	bin(j) = bin(j)+1
+        j = int((vector(i)-a)/h)!'locates' each of the vectors by rounding them up
+        bin(j) = bin(j)+1!adds 1 to the bin where the vector was located
+      end do
+      do i = 1,nbin
+        b(i) = b(i)/(N*h)!Normalizes the bins
+        x = a+dble(i)*(b-a)/dble(nbin)!Reparametrizes us to x
+        write(17,*) x, b(i)!Writes to a file
       end do
       end subroutine
